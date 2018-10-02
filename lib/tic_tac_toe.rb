1e942329c3c0868eb6f0d817db39f922ac848292
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-10-02 15:38:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,3 +20,52 @@ end
 def input_to_index(input)
   index = "#{input}".to_i - 1
 end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
+
+def position_taken?(board, index)
+  if (board[index] == nil || board[index] == " " || board[index] == "")
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    return true
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets
+  index = input_to_index(input)
+  until valid_move?(board, index)
+    puts "Invalid Move!"
+    puts "Please enter 1-9:"
+    input = gets
+    index = input_to_index(input)
+  end
+  move(board, index, value = "X")
+  display_board(board)
+end
+
+def turn_count(board)
+turncount = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turncount += 1
+    end
+  end
+  return turncount
+end
+ def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
